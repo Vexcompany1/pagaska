a8094de00d0f09220d8675a50b4b75912319d7b9
Vexcompany1 <98319106+Vexcompany1@users.noreply.github.com>
2025-03-12 11:09:55 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
+from instabot import Bot
+
 INSTAGRAM_ACCOUNTS = [
-    {"username": "akun1", "password": "password1"},
+    {"username": "sacrifice00199", "password": "monsters"},
     {"username": "akun2", "password": "password2"},
     # Tambahkan akun lain jika perlu
 ]
 
-TARGET_POST_URL = "https://www.instagram.com/p/XXXXXXXXX/"
+TARGET_POST_URL = "https://www.instagram.com/p/DHCp4kzSXaU/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
 LIKE_LIMIT = 50  # Maksimal 50 like per sesi
 SLEEP_TIME = 60  # Jeda 1 menit setiap 50 like
